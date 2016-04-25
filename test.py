c5d0809021b5e536d18bdbdbf283672f0704ca25
Alexandre Wimmers <adwimmers@ucla.edu>
2016-04-24 22:33:18 -0700

    No need to hard-abort on FB failure

--- test.py
+++ test.py
@@ -29,8 +29,7 @@ def main():
 	if run_command('javac ' + targetJFile) != 0:
 		sys.exit(1)
 	
-	if run_command('java -jar ' + jarLoc + ' ' + targetCFile) != 0:
-		sys.exit(1)
+	run_command('java -jar ' + jarLoc + ' ' + targetCFile)
 		
 	os.remove(targetCFile);
 
